542a650b4ed5d1528e18a9d77f91cdc0d6a9da6b
Gibriel Spiteri <GS4656@zebra.com>
2018-06-22 09:37:08 -0400

    Jeremy & Gibriel - Pulling information from the backend for display on the frontend, as well as uploading information from frontend to database

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const path = require('path');
 var csvParser = require('csv-parse');
 var fs = require('fs');
 var $ = jQuery = require('jQuery');
+function Emp(name, coreID, job, supervisor, employeeList){
+  this.name=name;
+  this.coreID = coreID;
+  this.job = job;
+  this.supervisor = supervisor;
+  this.employeeList = employeeList;
+}
+var all_people = []
+var faris = new Emp("","","","");
 require('./jquery-csv/src/jquery.csv.js');
 
 var app = express();
@@ -50,12 +59,18 @@ function handleDisconnect() {
       setTimeout(handleDisconnect, 2000);
     }
     console.log('Connected');
-    var table1 = "CREATE TABLE IF NOT EXISTS activity(activity_num INT AUTO_INCREMENT PRIMARY KEY, core_id VARCHAR(25), achievement VARCHAR(100), description VARCHAR(5000), points INT(2))";
+
+    var table1 = "CREATE TABLE IF NOT EXISTS activity(activityID INT AUTO_INCREMENT PRIMARY KEY, coreID VARCHAR(25), accompID INT(3), activity_desc VARCHAR(2500))";
     connection.query(table1, function(err, result) {
       if (err) throw err;
       console.log("Event table created");
     });
 
+    var accomplishments = "CREATE TABLE IF NOT EXISTS accomplishment(accompID INT AUTO_INCREMENT PRIMARY KEY, description VARCHAR(2500), points INT(2))";
+    connection.query(accomplishments, function(err, result) {
+      if (err) throw err;
+      console.log("Event table created");
+    });
   });
   connection.on('error', function(err) {
     console.log('db error', err);
@@ -69,7 +84,7 @@ function handleDisconnect() {
 
 handleDisconnect();
 
-var server = app.listen(3005, "10.61.204.94", function() {
+var server = app.listen(3005, "localhost", function() {
   var host = server.address().address;
   var port = server.address().port;
 
@@ -77,13 +92,93 @@ var server = app.listen(3005, "10.61.204.94", function() {
 })
 
 app.get('/', function(req, res) {
-  res.render('pages/index');
+  res.render('pages/newindex');
 });
 app.get('/ViewPoints', function(req, res) {
   res.render('pages/Points');
 });
 app.get('/ViewEmps', function(req, res) {
   res.render('pages/ViewEmps');
+});
+
+var response = "";
+var accomDescriptions = [];
+var accomPoints = [];
+app.post('/viewPoints', function(req, res) {
+
+  var empID = req.body.CORE_ID;
+  //var result = findPersonByID(empID, faris, []);
+  var personAccomps = [];
+  var query = "SELECT * FROM `activity` WHERE coreID="+ connection.escape(empID);
+  connection.query(query, function(err, accomplish) {
+    if (err) throw err;
+    for(accomplishmentTemp in accomplish)
+    {
+      personAccomps.push(accomplish[accomplishmentTemp]);
+    }
+    console.log(personAccomps);
+  });
+
+  setTimeout(function(){parseAccomplishments(personAccomps);},500);
+  // res.writeHead(200, {'Content-Type': 'text/html'});
+  // res.write('<html><head><link rel="stylesheet" href="/styles.css"></head><body>');
+  // res.write()
+  setTimeout(function(){
+    console.log("IN FINAL RESPONSE TIMEOUT");
+    console.log(personAccomps);
+    var pointCount = 0;
+    for(val in personAccomps)
+    {
+      if(val == 0){
+        response += "<h3>" + ;
+        response = "<table><tr><th>Accomplishment</th><th>Description</th><th>Points</th></tr>";
+
+      }
+      response += "<tr><td>";
+      response += accomDescriptions[val];
+      response += "</td><td>";
+      response += personAccomps[val].activity_desc;
+      response += "</td><td>";
+      response += accomPoints[val];
+      pointCount += accomPoints[val];
+      response += "</td></tr>";
+    }
+    response += "<tr><td></td><td>Total Points</td><td>"
+    response += pointCount;
+    response += "</td></tr>";
+    response += "</table";
+    res.send(response);
+  },700);
+
+});
+
+function parseAccomplishments(personAccomps){
+  accomDescriptions = [];
+  accomPoints = [];
+  for(accomplishLocation in personAccomps)
+  {
+    var accomp = "SELECT * FROM `accomplishment` WHERE accompID=" + personAccomps[accomplishLocation].accompID;
+    connection.query(accomp, function(err, res) {
+      if (err) throw err;
+      accomDescriptions.push(res[0].description);
+      accomPoints.push(res[0].points);
+    });
+  }
+}
+
+
+
+app.post('/addPoints', function(req, res) {
+  var CORE_ID =req.body.CORE_ID;
+  var ACCOMPLISHMENT = req.body.ACCOMPLISHMENT;
+  var DESCRIPTION = req.body.DESCRIPTION;
+
+  var points = "INSERT INTO `activity` (`coreID`, `accompID`, `activity_desc`) VALUES (" + connection.escape(CORE_ID) + "," + connection.escape(ACCOMPLISHMENT) + "," +connection.escape(DESCRIPTION) +");";
+  connection.query(points, function(err, result) {
+    if (err) throw err;
+    console.log("Inserted activity");
+  });
+  res.send("");
 });
 
 app.post('/add_csv', function(req, res) {
@@ -119,8 +214,7 @@ app.post('/add_csv', function(req, res) {
 
           connection.query(insert, function(err, row) {
             if (err) throw err;
-            var numRows = row.affectedRows;
-            console.log(index);
+            //console.log(index);
           });
         }
         console.log("Done");
@@ -128,3 +222,93 @@ app.post('/add_csv', function(req, res) {
     });
   });
 });
+
+function sortEmps(){
+
+  //Getting faris from the database by his 'emp_name'
+  var get_faris = "SELECT * FROM `employees` WHERE `emp_name` = 'Habbaba, Mr. Faris S (Faris)'";
+  connection.query(get_faris, function(err, res) {
+
+    if (err) throw err;
+    faris.name=res[0].emp_name;
+    faris.coreID = res[0].coreID;
+    faris.job = res[0].job;
+    faris.supervisor = res[0].supervisor;
+    faris.employeeList = [];
+    console.log(faris);
+  });
+
+  //Getting all the employees into a list(all_people)
+  var get_all = "SELECT * FROM `employees`";
+
+  connection.query(get_all, function(err, res) {
+    if (err) throw err;
+    for (var i in res) {
+      //Make a new employee using their information
+      var person = new Emp("","","","");
+      person.name=res[i].emp_name;
+      person.coreID = res[i].coreID;
+      person.job = res[i].job;
+      person.supervisor = res[i].supervisor;
+      person.employeeList = [];
+
+      //Push this to the list of all all_people
+      all_people.push(person);
+    }
+    //console.log(all_people);
+  });
+
+  //AFTER WE GET THE DATA, PASS THIS TO A NEW FUNCTION
+  //Getting the
+  setTimeout(function(){recurseList(faris,all_people);},3000);
+}
+var count = 0;
+function printFaris(faris){
+  console.log(faris.name);
+  printTree(faris, 0);
+  console.log(count);
+}
+
+function recurseList(person, all_people)
+{
+  for(val in all_people)
+  {
+    if(all_people[val].supervisor === person.name)
+    {
+      person.employeeList.push(all_people[val]);
+      recurseList(all_people[val], all_people);
+    }
+  }
+
+}
+
+function printTree(person, currentTabs){
+  for(emp in person.employeeList){
+
+    var tabs = "\t";
+    for(let i = 0; i < currentTabs; i++){
+      tabs += "\t";
+    }
+    count++;
+    console.log(tabs + person.employeeList[emp].name);
+    printTree(person.employeeList[emp], currentTabs+1);
+  }
+
+
+}
+
+function findPersonByID(coreID, person, result){
+  for(emp in person.employeeList){
+    if(person.employeeList[emp].coreID === coreID){
+      console.log("Manager: " + person.name);
+      console.log("Person: " + person.employeeList[emp].name);
+      result.push(person.name);
+      result.push(person.employeeList[emp]);
+    }
+    else{
+      findPersonByID(coreID, person.employeeList[emp],result);
+    }
+  }
+  return result;
+}
+sortEmps();
